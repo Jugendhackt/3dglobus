1327ea52db968674dda584d2a8a9d679ef2a6cb8
Charlotte <charlotte@example.com>
2022-12-03 15:50:02 +0000

    Nun Dreh-und Zoombar

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth-100, window.innerHeight-100 );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -16,15 +16,20 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+camera.position.set( 0, 20, 100 );
+controls.update();
 
 function animate() {
-    requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+	requestAnimationFrame( animate );
 
-    renderer.render( scene, camera );
-};
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
 
-animate();
+	renderer.render( scene, camera );
+
+}
+animate()
