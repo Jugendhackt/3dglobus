ab8ebabfedfd9bbf489f68198b1c8d321bd19824
Angelina <loki@example.com>
2022-12-04 13:38:59 +0100

    stecknadel griechenland und marokko und türkei

--- main.js
+++ main.js
@@ -92,6 +92,21 @@ loader.load( 'stecknadel Vereinigtes Königreich.glb', function ( gltf ) {
 }, undefined, function ( error ) {
 	console.error( error );
 } );
+loader.load( 'stecknadel Griechenland.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+loader.load( 'stecknadel Türkei.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
+loader.load( 'stecknadel Marokko.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
 
 
 
